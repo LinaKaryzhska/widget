137f5a3c656f77dd7a8214779d655cf3d74307f9
Polina Karyzhska <polinakaryzhska@gmail.com>
2023-10-26 11:02:30 +0300

    Fix errr

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import './styles.css';
 
-import headerHtml from './components/header.html?raw';
 import chatBoxHtml from './components/chatBox.html?raw';
 
-document.getElementById('header').innerHTML = headerHtml;
 document.getElementById('chatBox').innerHTML = chatBoxHtml;
